9d22108629c9cd00623e5a72aa6c0117ecb3ae0f
andreytopchu <andreytopchu01@mail.ru>
2021-02-28 13:17:26 +0200

    Add Linq (version 1.0)

---
+++ LINQ/LINQ/Node.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LINQ
+{
+   public class Node
+    {
+        public string Text { get; private set; }
+        public int SerialNumber { get; private set; }
+        public DateTime Date { get; private set; }
+
+        public Node(string text, int serialNumber, DateTime date)
+        {
+            if (serialNumber > 0)
+            {
+                this.Text = text;
+                this.SerialNumber = serialNumber;
+                this.Date = date;
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("№ {0,3}  Текст: {1}  Дата выполнения: {2}",SerialNumber,Text,Date.ToShortDateString());
+        }
+    }
+}
---
+++ LINQ/LINQ/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            Node[] node = new Node[100];
+            for (int i = 0; i < 100; i++)
+            {
+                node[i] = new Node(GenerateString(rand,7), i + 1, GenerateDate(rand));
+                Console.WriteLine(node[i]);
+            }
+            Console.ReadLine();
+        }
+
+        public static char GenerateChar(Random rng)
+        {
+            return (char)(rng.Next('a', 'z' + 1));
+        }
+
+        public static string GenerateString(Random rng, int length)
+        {
+            char[] letters = new char[length];
+            for (int i = 0; i < length; i++)
+            {
+                letters[i] = GenerateChar(rng);
+            }
+            return new string(letters);
+        }
+
+        public static DateTime GenerateDate(Random rnd)
+        {
+            DateTime start = new DateTime(2021, 2, 1);
+            int range = ((TimeSpan)(DateTime.Today - start)).Days;
+            return start.AddDays(rnd.Next(range));
+        }
+
+    }
+}
